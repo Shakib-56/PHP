18e7d203f2e09234f71dacef89d80e37d16954d8
Shakib <hossenmdshakib47@gmail.com>
2026-07-09 06:35:06 +0600

    foreachloop added

---
+++ foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For each</title>
+</head>
+<body>
+    <?php
+    $numbers=array(345,397,676,385,2657,5784);
+    foreach($numbers as $number){
+        echo $number ."<br>";
+    }
+    
+    
+    ?>
+</body>
+</html>
